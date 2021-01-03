7e406034337dad0b39c8b1b4a3076da2081ebc1e
DiuminPM <75372207+DiuminPM@users.noreply.github.com>
2021-01-03 14:30:43 +0300

    other variants

--- TrafficLite/ViewController.swift
+++ TrafficLite/ViewController.swift
@@ -6,6 +6,9 @@
 //
 
 import UIKit
+enum CurentLight {
+    case red, yellow, green
+}
 
 class ViewController: UIViewController {
     @IBOutlet var redColorView: UIView!
@@ -13,38 +16,66 @@ class ViewController: UIViewController {
     @IBOutlet var greenColorView: UIView!
     @IBOutlet var actionButton: UIButton!
     
+    private var currentLight = CurentLight.red
+    private var lighthIsOn: CGFloat = 1
+    private var lighthIsOff: CGFloat = 0.2
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        redColorView.backgroundColor = UIColor.red.withAlphaComponent(0.2)
-        yellowColorView.backgroundColor = UIColor.yellow.withAlphaComponent(0.2)
-        greenColorView.backgroundColor = UIColor.green.withAlphaComponent(0.2)
+        actionButton.layer.cornerRadius = 10
         redColorView.layer.cornerRadius = 75
         yellowColorView.layer.cornerRadius = 75
         greenColorView.layer.cornerRadius = 75
-        actionButton.layer.cornerRadius = 10
+        redColorView.alpha = lighthIsOff
+        yellowColorView.alpha = lighthIsOff
+        greenColorView.alpha = lighthIsOff
+        
+//        redColorView.backgroundColor = UIColor.red.withAlphaComponent(0.2)
+//        yellowColorView.backgroundColor = UIColor.yellow.withAlphaComponent(0.2)
+//        greenColorView.backgroundColor = UIColor.green.withAlphaComponent(0.2)
+//        redColorView.layer.cornerRadius = 75
+//        yellowColorView.layer.cornerRadius = 75
+//        greenColorView.layer.cornerRadius = 75
+//        actionButton.layer.cornerRadius = 10
         // Do any additional setup after loading the view.
     }
     @IBAction func makeButtonAction() {
-        if redColorView.backgroundColor == UIColor.red.withAlphaComponent(0.2) &&
-        yellowColorView.backgroundColor == UIColor.yellow.withAlphaComponent(0.2) &&
-        greenColorView.backgroundColor == UIColor.green.withAlphaComponent(0.2) {
-            self.redColorView.backgroundColor = UIColor.red
-            actionButton.setTitle("Next", for: .normal)
-        } else if redColorView.backgroundColor == UIColor.red {
-            self.redColorView.backgroundColor = UIColor.red.withAlphaComponent(0.2)
-            self.yellowColorView.backgroundColor = UIColor.yellow
+        actionButton.setTitle("Next", for:  .normal)
+        switch currentLight {
+        case .red:
+            greenColorView.alpha = lighthIsOff
+            redColorView.alpha = lighthIsOn
+            currentLight = .yellow
+        case .yellow:
+            redColorView.alpha = lighthIsOff
+            yellowColorView.alpha = lighthIsOn
+            currentLight = .green
+        case.green:
+            yellowColorView.alpha = lighthIsOff
+            greenColorView.alpha = lighthIsOn
+            currentLight = .red
         }
-        else if yellowColorView.backgroundColor == UIColor.yellow {
-            self.yellowColorView.backgroundColor = UIColor.yellow.withAlphaComponent(0.2)
-            self.greenColorView.backgroundColor = UIColor.green
-        }
-        else {
-            redColorView.backgroundColor = UIColor.red.withAlphaComponent(0.2)
-            yellowColorView.backgroundColor = UIColor.yellow.withAlphaComponent(0.2)
-            greenColorView.backgroundColor = UIColor.green.withAlphaComponent(0.2)
-            actionButton.setTitle("Start", for: .normal)
-        }
-        }
+//        if redColorView.backgroundColor == UIColor.red.withAlphaComponent(0.2) &&
+//        yellowColorView.backgroundColor == UIColor.yellow.withAlphaComponent(0.2) &&
+//        greenColorView.backgroundColor == UIColor.green.withAlphaComponent(0.2) {
+//            self.redColorView.backgroundColor = UIColor.red
+//            actionButton.setTitle("Next", for: .normal)
+//        } else if redColorView.backgroundColor == UIColor.red {
+//            self.redColorView.backgroundColor = UIColor.red.withAlphaComponent(0.2)
+//            self.yellowColorView.backgroundColor = UIColor.yellow
+//        }
+//        else if yellowColorView.backgroundColor == UIColor.yellow {
+//            self.yellowColorView.backgroundColor = UIColor.yellow.withAlphaComponent(0.2)
+//            self.greenColorView.backgroundColor = UIColor.green
+//        }
+//        else {
+//            redColorView.backgroundColor = UIColor.red.withAlphaComponent(0.2)
+//            yellowColorView.backgroundColor = UIColor.yellow.withAlphaComponent(0.2)
+//            greenColorView.backgroundColor = UIColor.green.withAlphaComponent(0.2)
+//            actionButton.setTitle("Start", for: .normal)
+//        }
+//        }
+    }
     }
         
         
